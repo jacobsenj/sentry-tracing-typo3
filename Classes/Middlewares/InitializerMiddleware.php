c1d27bf90c2f9cc7e8872ffef3cab122b42ffb9c
Jens Jacobsen <jens.jacobsen@ueberbit.de>
2023-12-07 13:27:02 +0100

    Remove readonly flag from InitializerMiddleware class

--- Classes/Middlewares/InitializerMiddleware.php
+++ Classes/Middlewares/InitializerMiddleware.php
@@ -24,7 +24,7 @@ use Sentry\Tracing\TransactionContext;
 
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 
-final readonly class InitializerMiddleware implements MiddlewareInterface
+final class InitializerMiddleware implements MiddlewareInterface
 {
     public function __construct(private ExtensionConfiguration $extensionConfiguration)
     {
